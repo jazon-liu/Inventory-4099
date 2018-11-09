875b8ec0acc8b1c717b0748dfeabf31d0a0a6327
MagicalTaterTot <shadowbox826@gmail.com>
2018-11-09 10:50:31 -0500

    modified items.json

--- static/main.js
+++ static/main.js
@@ -1,19 +1,8 @@
-/*========= TODO =========*/
-/* Use an API to automatically get csv from inventory sheet and convert to JSON */
-/* Automate collection of csv data when sheet is updated */
-// CSV URL (Copy): https://docs.google.com/a/team4099.com/spreadsheets/d/1YVnjXinLYBRPBY3uEe10vgiO9Z9fyTDe9Z6JLuPUliA/export?format=csv&id=1YVnjXinLYBRPBY3uEe10vgiO9Z9fyTDe9Z6JLuPUliA&gid=0
-
-// Once system is set up completely, just update file one last time and have people use that
-
-/*
-
-*/
-
 // Load modules
 const fs = require('fs');
 const path = require('path');
 const firebase = require('firebase');
-/*
+
 // Firebase Stuff
 let auth = JSON.parse(fs.readFileSync(path.join(__dirname, "../auth.json"), "utf8"))
 var config = {
@@ -29,7 +18,7 @@ let db = firebase.database();
 for (let i = 0; i < db; i++) {
   console.log(i)
 }
-*/
+
 
 // Load item file
 let items = JSON.parse(fs.readFileSync(path.join(__dirname, "../items.json"), "utf8"))
